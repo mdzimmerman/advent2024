154b5cd3a6c021c4e3815652df43a4af6dcf5e1e
Matthew Zimmerman <matt.zimmerman@ccri.com>
2024-12-10 09:46:46 -0500

    day 8 part 1

--- day08/day08.py
+++ day08/day08.py
@@ -18,16 +18,26 @@ class Antennas(CharArray):
             out[ch].append(pt)
         return out
 
-    def find_antinodes(self, p1: Point, p2: Point):
-        fs
-        delta = p2 - p1
-        a1 = p1 - delta
-        a2 = p2 + delta
-        return {a1, a2}
+    def find_antinodes(self):
+        antinodes = set()
+        for aname, apoints in self.antennas.items():
+            self.logger.debug(aname, apoints)
+            for i in range(len(apoints)-1):
+                for j in range(i+1, len(apoints)):
+                    pi = apoints[i]
+                    pj = apoints[j]
+                    self.logger.debug(pi, pj)
+                    delta = pj - pi
+                    antinodes.add(pi - delta)
+                    antinodes.add(pj + delta)
+        return set(filter(lambda p: self.in_bounds(p), antinodes))
 
 if __name__ == '__main__':
-    test = Antennas.from_file("test.txt")
-    antinodes = test.find_antinodes(test.antennas["A"][0], test.antennas["A"][1])
+    test = Antennas.from_file("test.txt", loglevel="DEBUG")
+    antinodes = test.find_antinodes()
     test.print(overset=antinodes)
-    for k, v in test.antennas.items():
-        print(k, v)
+    print(len(antinodes))
+
+    inp = Antennas.from_file("input.txt")
+    antinodes = inp.find_antinodes()
+    print(len(antinodes))
