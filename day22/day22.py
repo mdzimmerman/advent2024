24dd921a1da86fa7f4fec4f07d237bbd198cbf07
Matthew Zimmerman <matt.zimmerman@ccri.com>
2024-12-24 08:22:23 -0500

    day 22 part 1

---
+++ day22/day22.py
@@ -0,0 +1,77 @@
+import itertools
+import sys
+
+sys.path.append("..")
+from aoc import Logger
+
+PRUNE = 16777216
+
+def calc_next(x):
+    x = ((x * 64) ^ x) % PRUNE
+    x = ((x // 32) ^ x) % PRUNE
+    return ((x * 2048) ^ x) % PRUNE
+
+
+def calc_next_n(x0, n=2000):
+    x = x0
+    for _ in range(n):
+        x = calc_next(x)
+    return x
+
+def calc_iter(x0, n=2000):
+    x = x0
+    for _ in range(n+1):
+        yield x
+        x = calc_next(x)
+
+def part1(filename, logger=Logger("WARN")):
+    sum = 0
+    with open(filename, "r") as fh:
+        for l in fh:
+            x = int(l.strip())
+            x2k = calc_next_n(x, 2000)
+            logger.debug(f"{x}: {x2k}")
+            sum += x2k
+    return sum
+
+def get_prices(x0, n=2000):
+    return list(x % 10 for x in calc_iter(x0, n=n))
+
+def get_patterns(prices):
+    dprices = [0]
+    dprices.extend(b-a for a, b in itertools.pairwise(prices))
+    #print(prices)
+    #print(dprices)
+    pattern = dict()
+    for i in range(4, len(prices)):
+        d4 = tuple(dprices[i-3:i+1])
+        if d4 not in pattern:
+            pattern[d4] = prices[i]
+    return pattern
+
+def part2(filename):
+    with open(filename, "r") as fh:
+        for l in fh:
+            x = int(l.strip())
+            prices = get_prices(x, n=2000)
+
+if __name__ == '__main__':
+    print("-- test0 --")
+    test0 = 123
+    print(test0)
+    for _ in range(10):
+        test0 = calc_next(test0)
+        print(test0)
+
+    print()
+    print("-- test --")
+    print("part 1")
+    print(part1("test.txt", logger=Logger("DEBUG")))
+    print("part 2")
+    print(part2("test.txt"))
+
+
+    print()
+    print("-- input --")
+    print(part1("input.txt"))
+
